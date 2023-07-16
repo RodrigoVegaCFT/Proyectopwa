2170744283356515bedf2d62a2518f6191552701
rodrigoVega <seock.vega@gmail.com>
2023-07-16 17:51:56 -0400

    cuento solo en /cuento

--- src/pags/cuento.js
+++ src/pags/cuento.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { cuento } from './crear';
 
 function Cuento() {
   const navigate = useNavigate();
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const handleButtonClick = () => {
     navigate('/crear');
   };
+
   const handleClick = () => {
     const value = new SpeechSynthesisUtterance(cuento);
-    window.speechSynthesis.speak(value)
+    window.speechSynthesis.speak(value);
+    setIsPlaying(true);
   };
 
   useEffect(() => {
@@ -18,12 +21,33 @@ function Cuento() {
 
     return () => {
       document.body.classList.remove('background-image');
+      window.speechSynthesis.cancel(); // Detener la reproducción del cuento al salir del componente
     };
   }, []);
 
+  useEffect(() => {
+    if (isPlaying) {
+      const speechSynthesis = window.speechSynthesis;
+
+      speechSynthesis.addEventListener('end', handleSpeechEnd);
+      speechSynthesis.addEventListener('error', handleSpeechEnd);
+    }
+
+    return () => {
+      const speechSynthesis = window.speechSynthesis;
+
+      speechSynthesis.removeEventListener('end', handleSpeechEnd);
+      speechSynthesis.removeEventListener('error', handleSpeechEnd);
+    };
+  }, [isPlaying]);
+
+  const handleSpeechEnd = () => {
+    setIsPlaying(false);
+  };
+
   // Obtener el cuento de las propiedades de ubicación (location)
 
-  console.log(cuento);
+  
   return (
     <>
       <div className='fondo-cuento'>
@@ -32,16 +56,16 @@ function Cuento() {
             <h1>¡Disfruta tu cuento!</h1>
           </div>
         </section>
-          
         
         <section className="volver">
           <div className='btns'>
-            <button className="boton-volver" onClick={handleClick}>Escuchar cuento</button>
-            </div>
-            <div className='btns'>
+            <button className="boton-volver" onClick={handleClick} disabled={isPlaying}>
+              {isPlaying ? 'Reproduciendo...' : 'Escuchar cuento'}
+            </button>
+          </div>
+          <div className='btns'>
             <button className="boton-volver" onClick={handleButtonClick}>Volver a selecciones</button>
             </div>
-
         </section>
       </div>
     </>
